a463cfe685d8a766e47796eb6a8f161e714d3d87
pattiestarfish <61345949+pattiestarfish@users.noreply.github.com>
2020-03-31 16:53:09 -0700

    Minimax tutorials - adv:connect four

---
+++ machine learning/Minimax/adv/codeacad_evaluation_funct.py
@@ -0,0 +1,108 @@
+def evaluate_board(board):
+    if has_won(board, "X"):
+      return float("Inf")
+    elif has_won(board, "O"):
+      return -float("Inf")
+    else:
+      x_streaks = count_streaks(board, "X")
+      o_streaks = count_streaks(board, "O")
+      return x_streaks - o_streaks
+
+def count_streaks(board, symbol):
+    count = 0
+    for col in range(len(board)):
+        for row in range(len(board[0])):
+            if board[col][row] != symbol:
+                continue
+            # right
+            if col < len(board) - 3:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col + i][row] == symbol:
+                        num_in_streak += 1
+                    elif board[col + i][row] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #left
+            if col > 2:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col - i][row] == symbol:
+                        num_in_streak += 1
+                    elif board[col - i][row] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #up-right
+            if col < len(board) - 3 and row > 2:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col + i][row - i] == symbol:
+                        num_in_streak += 1
+                    elif board[col + i][row - i] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #down-right
+            if col < len(board) - 3 and row < len(board[0]) - 3:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col + i][row + i] == symbol:
+                        num_in_streak += 1
+                    elif board[col + i][row + i] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #down-left
+            if col > 2 and row < len(board[0]) - 3:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col - i][row + i] == symbol:
+                        num_in_streak += 1
+                    elif board[col - i][row + i] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #up-left
+            if col > 2 and row > 2:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col - i][row - i] == symbol:
+                        num_in_streak += 1
+                    elif board[col - i][row - i] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #down-left
+            if col > 2 and row < len(board[0]) - 3:
+                num_in_streak = 0
+                for i in range(4):
+                    if board[col - i][row + i] == symbol:
+                        num_in_streak += 1
+                    elif board[col - i][row + i] != " ":
+                        num_in_streak = 0
+                        break
+                count += num_in_streak
+            #down
+            num_in_streak = 0
+            if row < len(board[0]) - 3:
+                for i in range(4):
+                    if row + i < len(board[0]):
+                        if board[col][row + i] == symbol:
+                            num_in_streak += 1
+                        else:
+                            break
+            for i in range(4):
+                if row - i > 0:
+                    if board[col][row - i] == symbol:
+                        num_in_streak += 1
+                    elif board[col][row - i] == " ":
+                        break
+                    else:
+                        num_in_streak == 0
+            if row < 3:
+                if num_in_streak + row < 4:
+                    num_in_streak = 0
+            count += num_in_streak
+    return count
